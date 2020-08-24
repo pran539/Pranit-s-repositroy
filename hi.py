532ceb52dd06060b38162b408466de7ea061209c
pran539 <69524234+pran539@users.noreply.github.com>
2020-08-24 09:43:44 +0530

    Create hi.py

---
+++ hi.py
@@ -0,0 +1,19 @@
+"""tkinter=package
+name=root
+
+
+"""
+import pygame
+import label
+import tkinter
+from tkinter import ttk
+import tkinter as tk
+
+root=tkinter.Tk()
+root.title("yar")
+root.mainloop()
+root=tk.Tk
+"""
+
+"""
+tkinter .label(root, text='enter your name')
